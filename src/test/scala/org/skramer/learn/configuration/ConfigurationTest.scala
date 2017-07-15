dd137fa58f18e322b82a644fae422b867e1e6e77
Sebastian Kramer <sebastian.kramer@wp.pl>
2017-07-15 12:58:06 +0200

    added configuration tests

---
+++ src/test/scala/org/skramer/learn/configuration/ConfigurationTest.scala
@@ -0,0 +1,49 @@
+package org.skramer.learn.configuration
+
+import akka.actor.ActorSystem
+import com.typesafe.config.{Config, ConfigFactory}
+import org.scalatest._
+
+class ConfigurationTest extends fixture.WordSpec with Matchers {
+  override type FixtureParam = Config
+
+  "config factory" should {
+    "read default config" in { cf =>
+      cf.getString("AkkaLearning.envVar") shouldBe "skramer"
+    }
+
+    "read config subtree" in { cf =>
+      val nestedConfig = cf.getConfig("AkkaLearning.Nested")
+      nestedConfig.getString("otherVar") shouldBe "skramer"
+    }
+
+    "read included conf" in { cf =>
+      val includedIntValue = cf.getInt("Included.IntValue")
+      includedIntValue shouldBe 20
+    }
+
+    "read overridden attributes" in { cf =>
+      val overridenValue = cf.getString("toBeOverridden")
+      overridenValue shouldBe "thisIsAnOverride"
+    }
+
+    "read overridden attributes from included file" in { cf =>
+      val overridenValue = cf.getString("toBeOverriddenFromInclude")
+      overridenValue shouldBe "thisIsAnOverrideOfIncludedProperty"
+    }
+
+    "read overriden attributes from lifted config" in { cf =>
+      val liftedConfigWithFallback = cf.getConfig("SubApp").withFallback(cf)
+      liftedConfigWithFallback.getString("AkkaLearning.envVar") shouldBe "skramer"
+      liftedConfigWithFallback.getString("AkkaLearning.Nested.otherVar") shouldBe "overrideFromLiftedConf"
+    }
+
+  }
+
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    val config = ConfigFactory.load("myApplicationConf")
+    val system = ActorSystem("testSystem", config)
+
+    withFixture(test.toNoArgTest(system.settings.config))
+  }
+}
