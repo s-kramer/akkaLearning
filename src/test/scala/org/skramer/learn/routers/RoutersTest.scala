aacad352956eee51884271967e7439cc346e5cba
Sebastian Kramer <sebastian.kramer@wp.pl>
2017-07-28 19:12:19 +0200

    simple routing tests

---
+++ src/test/scala/org/skramer/learn/routers/RoutersTest.scala
@@ -0,0 +1,83 @@
+package org.skramer.learn.routers
+
+import akka.actor.{ActorRef, Props}
+import akka.routing._
+import akka.testkit.TestProbe
+import org.skramer.learn.AkkaLearningTestTrait
+import org.skramer.learn.forwardingActor.ForwardingActor
+
+import scala.collection.mutable.ListBuffer
+
+class RoutersTest extends AkkaLearningTestTrait {
+  "router created from source code" should {
+    "spawn its children and send messages to them" in {
+      val probe = TestProbe()
+
+      val router = system.actorOf(RoundRobinPool(5).props(Props(new ForwardingActor(probe.ref))), "roundRobinRouter")
+
+      router ! 1
+      router ! 2
+      router ! 3
+      router ! 4
+      router ! 5
+
+      val senders = ListBuffer[ActorRef]()
+      val numbers = ListBuffer[Int]()
+      for (i <- 1 to 5) {
+        probe.expectMsgPF() {
+          case msg: Int => senders += probe.lastSender; numbers += msg
+        }
+      }
+      numbers should contain allElementsOf (1 to 5)
+      senders should have size 5
+    }
+  }
+
+  "any router" should {
+    "propagate broadcast to all routees" in {
+      val probes = List.fill(5) { TestProbe() }
+
+      val router = system.actorOf(RoundRobinGroup(probes.map { _.ref.path.toString }).props(), "roundRobinGroupRouter")
+
+      router ! Broadcast(1)
+
+      probes.foreach { p => p.expectMsg(1) }
+    }
+  }
+
+  "broadcasting router " should {
+    "send every message to all the routees" in {
+      val probe1 = TestProbe("probe1")
+      val probe2 = TestProbe("probe2")
+      val path = probe1.ref.path
+
+      val router = system.actorOf(BroadcastGroup(List(probe1.ref.path.toString, probe2.ref.path.toString))
+                                  .props(), "broadcastingGroup")
+
+      router ! 1
+      router ! 2
+
+      val probe1Msgs = probe1.receiveN(2)
+      val probe2Msgs = probe2.receiveN(2)
+      probe1Msgs should contain allElementsOf (1 to 2)
+      probe2Msgs should contain allElementsOf (1 to 2)
+    }
+  }
+
+  "router created from configuration" should {
+    "spawn its children and send messages to them" in {
+      val probe = TestProbe()
+
+      val router = system.actorOf(FromConfig().props(Props(new ForwardingActor(probe.ref))), "balancingPool")
+
+      router ! 1
+      router ! 2
+      router ! 3
+      router ! 4
+      router ! 5
+
+      val receivedMessages = probe.receiveN(5)
+      receivedMessages.map(_.asInstanceOf[Int]) should contain allElementsOf (1 to 5)
+    }
+  }
+}
